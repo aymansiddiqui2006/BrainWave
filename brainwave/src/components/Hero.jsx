6999234cf2a9b471292ca74e8a07bac8f0d1cfef
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-07-05 22:52:21 +0530

    feat: Add Generating, Notification, and CompanyLogos components; update Hero and Section for integration

--- brainwave/src/components/Hero.jsx
+++ brainwave/src/components/Hero.jsx
@@ -7,6 +7,9 @@ import { robot } from '../assets'
 import { BackgroundCircles, BottomLine, Gradient } from './design/Hero.jsx'
 import { heroIcons } from '../constants/index.js'
 import { ScrollParallax } from "react-just-parallax";
+import Generating from './Generating.jsx'
+import Notification from './Notification.jsx'
+import CompanyLogos from './CompanyLogos.jsx'
 
 function Hero() {
     const parallaxRef = useRef(null);
@@ -20,8 +23,8 @@ function Hero() {
         >
             <div className="container relative" ref={parallaxRef}>
                 <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
-                    <h1 className='h1 mb-6'>Explore the Possibilities of AI Chatting with
-                        <span className='inline-block relative ml-2'>Brainwave <img src={curve} className='top-full absolute left-0 w-full xl:-mt-2 ' width={624} height={28} alt='curve' /></span>
+                    <h1 className='h1 mb-6'>Explore the Possibilities  of&nbsp; AI&nbsp; Chatting with {" "}
+                        <span className='inline-block relative '>Brainwave{" "} <img src={curve} className='top-full absolute left-0 w-full xl:-mt-2 ' width={624} height={28} alt='curve' /></span>
                     </h1>
                     <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
                         Unleash the power of AI within Brainwave. Upgrade your productivity
@@ -41,12 +44,17 @@ function Hero() {
                                     height={490}
                                     alt="AI"
                                 />
+                                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2 "/>
                                 <ScrollParallax isAbsolutelyPositioned>
                                     <ul className='hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex'>
                                         {heroIcons.map((icon, index) => (
                                             <li className='p-5' key={index}><img src={icon} alt={icon} height={25} width={24} /></li>
                                         ))}
                                     </ul>
+                                </ScrollParallax>
+
+                                <ScrollParallax isAbsolutelyPositioned>
+                                    <Notification className=" hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex" title="Code generation"/>
                                 </ScrollParallax>
                             </div>
 
@@ -62,7 +70,9 @@ function Hero() {
                     </div>
                     <BackgroundCircles />
                 </div>
+                <CompanyLogos className="hidden relative z-10 mt-20 lg:block"/>
             </div>
+            <BottomLine/>
         </Section>
     )
 }
