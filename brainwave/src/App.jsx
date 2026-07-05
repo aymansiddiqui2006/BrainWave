363d98c7281dddba4bc5ab2f86ede14f9f5bc10d
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-07-05 20:26:01 +0530

    feat: Add core components and design elements for Brainwave application
    - Implemented Button component for consistent button styling and functionality.
    - Created Header component with navigation and responsive design.
    - Developed Hero section with engaging visuals and call-to-action.
    - Introduced Section component for structured layout with optional SVG decorations.
    - Added design elements for Benefits, Collaboration, Header, Hero, Pricing, Roadmap, and Services.
    - Established constants for navigation, hero icons, notifications, and pricing plans.
    - Configured Tailwind CSS for styling and responsive design.
    - Set up Vite for development environment with React support.

---
+++ brainwave/src/App.jsx
@@ -0,0 +1,19 @@
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from './components/Button.jsx'
+import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradient />
+    </>
+  )
+}
+
+export default App
