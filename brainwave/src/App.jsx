c93d4c4d22cc3268895f608458ebe8c52d4758ff
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-07-06 23:44:00 +0530

    services page done (with service1)

--- brainwave/src/App.jsx
+++ brainwave/src/App.jsx
@@ -4,6 +4,7 @@ import Button from './components/Button.jsx'
 import Header from './components/Header.jsx'
 import Hero from './components/Hero.jsx'
 import Collaboration from './components/Collaboration.jsx'
+import Services from './components/Services.jsx'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Hero />
         <Benefits/>
         <Collaboration/>
+        <Services/>
       </div>
       <ButtonGradient />
     </>
