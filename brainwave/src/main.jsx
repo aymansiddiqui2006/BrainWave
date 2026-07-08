bd079aa3270834e6d1369038e1648496d4d811db
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-07-08 20:05:15 +0530

    refactor: simplify routing setup by using BrowserRouter directly

--- brainwave/src/main.jsx
+++ brainwave/src/main.jsx
@@ -2,20 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter as Router  } from 'react-router-dom'
 
 
-const routes = createBrowserRouter([
-  {
-    path:'/',
-    element:<App/>
-  }
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routes}>
+    <Router>
       <App />
-    </RouterProvider>
+    </Router>
   </StrictMode>,
 )
